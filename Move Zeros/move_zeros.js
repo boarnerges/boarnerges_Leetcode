8ebd4c3110a7fce4aa99f5cbacf44e6637ccd1de
boarnerges <olujawo1996@gmail.com>
2025-07-11 09:53:08 +0100

    alternative solution

--- Move Zeros/move_zeros.js
+++ Move Zeros/move_zeros.js
@@ -14,3 +14,22 @@ var moveZeroes = function (nums) {
     }
   }
 };
+
+//Alternative solution
+
+function moveZeroes(nums) {
+  let i = 0;
+
+  // Move non-zero elements to the front
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== 0) {
+      nums[i] = nums[j];
+      i++;
+    }
+  }
+
+  // Fill remaining positions with zeros
+  for (; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+}
